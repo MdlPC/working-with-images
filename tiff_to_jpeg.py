f2a96fcf96c514e1d4865e005fcca64816c3d097
MdlPC <56652951+MdlPC@users.noreply.github.com>
2020-02-25 11:59:08 +0100

    First steps in image format changing
    This small piece of code saves new copies of tiff files within a folder.

---
+++ tiff_to_jpeg.py
@@ -0,0 +1,26 @@
+import os
+from PIL import Image
+from tkinter import filedialog
+
+def tiff_to_jpeg(calidad=75):
+    directorio = filedialog.askdirectory()
+
+    for archivo in os.listdir(directorio):
+        if archivo.endswith('.tif'):
+            fin=archivo.index('.')
+            nombre=''
+            nombre_final = ''
+            for letra in range(0,fin):
+                nombre += archivo[letra]
+            nombre_final = nombre + '.jpg'
+            try:
+                print(f'transformando {nombre}')
+                imagen = Image.open(os.path.join(directorio,archivo))
+                imagen.thumbnail(imagen.size)
+                directorio_final = os.path.join(directorio, nombre_final)
+                imagen.save(directorio_final, format='JPEG', quality=calidad)
+            except:
+                pass
+        
+
+tiff_to_jpeg()
